bf787976fd1e1eadb4c59eacc02fbb3b72537f0d
Elijah Meeks <emeeks@stanford.edu>
2011-11-22 10:09:32 -0800

    Includes license, cleans things up a bit, search now looks at Label and ID fields--1.0 official release, or so I think

--- TubeMapLayout/src/org/elijahmeeks/tubemap/TubeMapLayout.java
+++ TubeMapLayout/src/org/elijahmeeks/tubemap/TubeMapLayout.java
@@ -1,14 +1,49 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+Copyright 2011 Elijah Meeks
+Authors : Elijah Meeks <emeeks@stanford.edu>
+Website : http://dhs.stanford.edu
+
+
+DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+Copyright 2011 Elijah Meeks. All rights reserved.
+
+The contents of this file are subject to the terms of either the GNU
+General Public License Version 3 only ("GPL") or the Common
+Development and Distribution License("CDDL") (collectively, the
+"License"). You may not use this file except in compliance with the
+License. You can obtain a copy of the License at
+http://gephi.org/about/legal/license-notice/
+or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+specific language governing permissions and limitations under the
+License.  When distributing the software, include this License Header
+Notice in each file and include the License files at
+/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+License Header, with the fields enclosed by brackets [] replaced by
+your own identifying information:
+"Portions Copyrighted [year] [name of copyright owner]"
+
+If you wish your version of this file to be governed by only the CDDL
+or only the GPL Version 3, indicate your decision by adding
+"[Contributor] elects to include this software in this distribution
+under the [CDDL or GPL Version 3] license." If you do not indicate a
+single choice of license, a recipient has the option to distribute
+your version of this file under either the CDDL, the GPL Version 3 or
+to extend the choice of license to its licensees as provided above.
+However, if you add GPL Version 3 code and therefore, elected the GPL
+Version 3 license, then the option applies only if the new code is
+made subject to such option by the copyright holder.
+
+Contributor(s):
+
+ * 
+ * 
+ * 
  */
 package org.elijahmeeks.tubemap;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import org.gephi.graph.api.Graph;
-import org.gephi.graph.api.Node;
 import org.gephi.layout.plugin.AbstractLayout;
 import org.gephi.layout.spi.Layout;
 
@@ -16,19 +51,8 @@ import org.gephi.layout.spi.LayoutBuilder;
 import org.gephi.layout.spi.LayoutProperty;
 import org.openide.util.NbBundle;
 
-//New Stuff
-import java.io.File;
-import java.io.IOException;
-import org.gephi.data.attributes.api.AttributeColumn;
 import org.gephi.data.attributes.api.AttributeController;
-import org.gephi.data.attributes.api.AttributeModel;
 
-import org.gephi.graph.api.DirectedGraph;
-import org.gephi.graph.api.Edge;
-import org.gephi.graph.api.GraphController;
-import org.gephi.graph.api.GraphModel;
-import org.gephi.graph.api.GraphView;
-import org.gephi.graph.api.UndirectedGraph;
 import org.openide.util.Lookup;
 
 
@@ -76,11 +100,13 @@ public class TubeMapLayout extends AbstractLayout implements Layout {
         super(layoutBuilder);
     }
 
+    @Override
     public void initAlgo() {
         converged = false;
         graph = graphModel.getGraphVisible();
     }
 
+    @Override
     public void goAlgo() {
         graph = graphModel.getGraphVisible();
 ///////////////////////////////////////////////////////
@@ -106,7 +132,7 @@ public class TubeMapLayout extends AbstractLayout implements Layout {
         int index = 0;
         for (Node s : graph.getNodes()) {
             indicies.put(index, s);
-            if (s.getAttributes().getValue("Label").equals(targetID)) {
+            if (s.getAttributes().getValue("Label").equals(targetID) || s.getAttributes().getValue("ID").equals(targetID)) {
                 suitableTarget = true;
                 targetNode = index;
                 if (useXY == false){
@@ -210,9 +236,11 @@ public class TubeMapLayout extends AbstractLayout implements Layout {
         }
     }
 
+    @Override
     public void endAlgo() {
     }
 
+    @Override
     public LayoutProperty[] getProperties() {
         List<LayoutProperty> properties = new ArrayList<LayoutProperty>();
         try {
@@ -265,11 +293,11 @@ public class TubeMapLayout extends AbstractLayout implements Layout {
             
 
         } catch (Exception e) {
-            e.printStackTrace();
         }
         return properties.toArray(new LayoutProperty[0]);
     }
 
+    @Override
     public void resetPropertiesValues() {
     }
 
